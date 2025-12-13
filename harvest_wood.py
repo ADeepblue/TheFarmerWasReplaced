5f5e7e5f26483e3963ccbfe9a964ba34532cc1fe
Adeepblue <2631634050@qq.com>
2025-12-13 18:37:54 +0800

    harvest_wood

---
+++ harvest_wood.py
@@ -0,0 +1,21 @@
+# 1.5v
+## 说明
+# 种植灌木并收获灌木
+
+from __builtins__ import *
+
+clear()
+
+while True:
+	move(North)
+	plant(Entities.Bush)
+	if can_harvest():
+		harvest()
+	move(North)
+	plant(Entities.Bush)
+	if can_harvest():
+		harvest()
+	move(North)
+	plant(Entities.Bush)
+	if can_harvest():
+		harvest()
