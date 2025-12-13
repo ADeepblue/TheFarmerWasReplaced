7d4e30191d79755d9932b84f732efaba419c2a81
Adeepblue <2631634050@qq.com>
2025-12-13 23:13:31 +0800

    最新版本的草木混种,无法密植,会影响效率

--- harvest_wood.py
+++ harvest_wood.py
@@ -1,21 +1,39 @@
-# 1.5v
-## 说明
-# 种植灌木并收获灌木
+# 种植灌木并收获灌木,顺带收获草
 
 from __builtins__ import *
-
+from utils import *
+from utils import safe_harvest
 clear()
+size_num = get_world_size()
+at_least_percent = 0.9
+
+def do(line_index,row_index,size_num):
+	if (line_index+row_index) % 2 == 0:
+		plant(Entities.Tree)
+	else:
+		safe_harvest()
+
+	if get_water() <=at_least_percent:
+		use_item(Items.Water)
+
+
+
+# till the field in second row
+for line_index in range(size_num):
+	for row_index in range(size_num):
+		safe_harvest()
+		if (line_index+row_index) % 2 == 0:
+			till()
+
+		move(North)
+	move(East)
+
+#main loop
 
 while True:
-	move(North)
-	plant(Entities.Bush)
-	if can_harvest():
-		harvest()
-	move(North)
-	plant(Entities.Bush)
-	if can_harvest():
-		harvest()
-	move(North)
-	plant(Entities.Bush)
-	if can_harvest():
-		harvest()
+	for line_index in range(size_num):
+		for row_index in range(size_num):
+			safe_harvest()
+			do(line_index,row_index,size_num)
+			move(North)
+		move(East)
