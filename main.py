0057b75726e5c4f67e04cf317d44b998cd053fb1
Adeepblue <2631634050@qq.com>
2025-12-13 17:22:11 +0800

    胡萝卜,灌木,木材,all in one,v1.16

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-# 1.5v
-## 说明
-# 种植胡萝卜并收获胡萝卜
+# 1.6v
+# till the field, plant the carrot and get hay and wood
 
 from __builtins__ import *
-
 clear()
+size_num = get_world_size()
 
-till()
-move(North)
-till()
-move(North)
-till()
-move(North)
+# till the field in second row
+move(East)
+for index in range(size_num):
+    till()
+    move(North)
 
+# back to the (0,0)
+move(West)
+
+#main loop
 while True:
-    move(North)
-    plant(Entities.Carrot)
-    if can_harvest():
-        harvest()
-    move(North)
-    plant(Entities.Carrot)
-    if can_harvest():
-        harvest()
-    move(North)
-    plant(Entities.Carrot)
-    if can_harvest():
-        harvest()
+    for line_index in range(size_num):
+        for row_index in range(size_num):
+            if line_index == 0:
+                if can_harvest():
+                    harvest()
+                plant(Entities.Bush)
+                move(North)
+            elif line_index == 1:
+                if can_harvest():
+                    harvest()
+                plant(Entities.Carrot)
+                move(North)
+            else:
+                if can_harvest():
+                    harvest()
+                move(North)
+
+
+        move(East)
