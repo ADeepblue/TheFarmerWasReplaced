9fec6efdd272554f15593f7a2c96dabcd6bf7fc2
Adeepblue <2631634050@qq.com>
2025-12-13 13:23:25 +0800

    main文件,但是上下需要分开执行

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+# 1.4v
+## 说明
+# 种植灌木并收获灌木
+
 from __builtins__ import *
 
-while True:
-    # 场地增加后 往北移动,检测到边缘后自动返回原点重新向北移动
-    move(North)
-    # 检测收获函数
+# plant Bush
 
+while True:
+
+    move(North)
+    plant(Entities.Bush)
+
+# harvest wood
+
+while True:
+    move(North)
     if can_harvest():
         harvest()
