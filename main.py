7ade8e1d603a0cc3ca6f0f8071a1311acaed6597
Adeepblue <2631634050@qq.com>
2025-12-13 14:02:16 +0800

    自动化收获灌木main

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-# 1.4v
+# 1.5v
 ## 说明
 # 种植灌木并收获灌木
 
 from __builtins__ import *
 
-# plant Bush
+clear()
 
 while True:
-
     move(North)
     plant(Entities.Bush)
-
-# harvest wood
-
-while True:
+    if can_harvest():
+        harvest()
     move(North)
+    plant(Entities.Bush)
+    if can_harvest():
+        harvest()
+    move(North)
+    plant(Entities.Bush)
     if can_harvest():
         harvest()
