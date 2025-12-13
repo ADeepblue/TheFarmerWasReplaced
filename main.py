bda8403c7bcf4d053e9cc0c4d121f3fef1bfc509
Adeepblue <2631634050@qq.com>
2025-12-13 14:08:51 +0800

    自动收获胡萝卜

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-# 1.4v
+# 1.5v
 ## 说明
-# 种植灌木并收获灌木
+# 种植胡萝卜并收获胡萝卜
 
 from __builtins__ import *
 
 clear()
 
+till()
+move(North)
+till()
+move(North)
+till()
+move(North)
+
 while True:
     move(North)
-    plant(Entities.Bush)
+    plant(Entities.Carrot)
     if can_harvest():
         harvest()
     move(North)
-    plant(Entities.Bush)
+    plant(Entities.Carrot)
     if can_harvest():
         harvest()
     move(North)
-    plant(Entities.Bush)
+    plant(Entities.Carrot)
     if can_harvest():
         harvest()
