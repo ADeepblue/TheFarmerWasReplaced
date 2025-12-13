1402885017b6f3a8ae3fec4975ffd9b397da081c
Adeepblue <2631634050@qq.com>
2025-12-13 23:26:57 +0800

    函数库

--- utils.py
+++ utils.py
@@ -1,20 +1,19 @@
 
 from __builtins__ import *
 
-def single_do(line_index,row_index,size_num):
+def main_do(line_index,row_index,size_num):
 	if (line_index+row_index) % 2 == 0:
 		if line_index < size_num // 2:
 			plant(Entities.Tree)
 		else:
 			plant(Entities.Carrot)
 	else:
-		save_harvest()
+		safe_harvest()
 
 	if get_water() <=0.6:
 		use_item(Items.Water)
 
 
-
-def save_harvest():
+def safe_harvest():
 	if can_harvest():
 		harvest()
