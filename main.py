1f7e1d2f5e750ed58739320e0d4b43baaed1c605
Adeepblue <2631634050@qq.com>
2025-12-13 23:26:08 +0800

    进行了函数分离

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-# 1.6v
+# 1.8v
 # till the field, plant the carrot and get hay and wood
 
 from __builtins__ import *
+from utils import *
 clear()
 size_num = get_world_size()
 
-
-def single_do(line_index,row_index,size_num):
-	if (line_index+row_index) % 2 == 0:
-		if line_index < size_num // 2:
-			plant(Entities.Tree)
-		else:
-			plant(Entities.Carrot)
-	else:
-		save_harvest()
-
-	if get_water() <=0.5:
-		use_item(Items.Water)
-
-def save_harvest():
-	if can_harvest():
-		harvest()
 
 # till the field in second row
 for line_index in range(size_num):
@@ -28,7 +13,7 @@ for line_index in range(size_num):
 		if (line_index+row_index) % 2 == 0:
 			till()
 		else:
-			save_harvest()
+			safe_harvest()
 		move(North)
 	move(East)
 
@@ -36,7 +21,7 @@ for line_index in range(size_num):
 while True:
 	for line_index in range(size_num):
 		for row_index in range(size_num):
-			save_harvest()
-			single_do(line_index,row_index,size_num)
+			safe_harvest()
+			main_do(line_index, row_index, size_num)
 			move(North)
 		move(East)
