2203400ed0525bace88786fb3d994442341a3fb5
Adeepblue <2631634050@qq.com>
2025-12-13 18:38:37 +0800

    更新乔木为树

--- main.py
+++ main.py
@@ -5,33 +5,38 @@ from __builtins__ import *
 clear()
 size_num = get_world_size()
 
-# till the field in second row
-move(East)
-for index in range(size_num):
-    till()
-    move(North)
 
-# back to the (0,0)
-move(West)
+def single_do(line_index,row_index,size_num):
+	if (line_index+row_index) % 2 == 0:
+		if line_index < size_num // 2:
+			plant(Entities.Tree)
+		else:
+			plant(Entities.Carrot)
+	else:
+		save_harvest()
+
+	if get_water() <=0.5:
+		use_item(Items.Water)
+
+def save_harvest():
+	if can_harvest():
+		harvest()
+
+# till the field in second row
+for line_index in range(size_num):
+	for row_index in range(size_num):
+		if (line_index+row_index) % 2 == 0:
+			till()
+		else:
+			save_harvest()
+		move(North)
+	move(East)
 
 #main loop
 while True:
-    for line_index in range(size_num):
-        for row_index in range(size_num):
-            if line_index == 0:
-                if can_harvest():
-                    harvest()
-                plant(Entities.Bush)
-                move(North)
-            elif line_index == 1:
-                if can_harvest():
-                    harvest()
-                plant(Entities.Carrot)
-                move(North)
-            else:
-                if can_harvest():
-                    harvest()
-                move(North)
-
-
-        move(East)
+	for line_index in range(size_num):
+		for row_index in range(size_num):
+			save_harvest()
+			single_do(line_index,row_index,size_num)
+			move(North)
+		move(East)
