331fca0261552f4714f94fcdfcf592d125a6212b
Adeepblue <2631634050@qq.com>
2025-12-14 22:21:26 +0800

    增加了to_position函数

--- utils.py
+++ utils.py
@@ -25,3 +25,20 @@ def till_all_field(size_num):
 			till()
 			move(North)
 		move(East)
+
+def to_position(x,y):
+	x_now = get_pos_x()
+	y_now = get_pos_y()
+	if x_now < x:
+		for _ in range(x-x_now):
+			move(East)
+	elif x_now > x:
+		for _ in range(x_now-x):
+			move(West)
+
+	if y_now < y:
+		for _ in range(y-y_now):
+			move(North)
+	elif y_now > y:
+		for _ in range(y_now-y):
+			move(South)
