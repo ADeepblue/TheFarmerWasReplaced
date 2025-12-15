dbce5d437013e3405a4680da12fe953b4146fe4f
Adeepblue <2631634050@qq.com>
2025-12-15 14:54:22 +0800

    单行种植向日葵

---
+++ harvest_energy.py
@@ -0,0 +1,45 @@
+from __builtins__ import *
+from utils import *
+
+# main
+clear()
+to_position((0,0))
+
+world_size = get_world_size()
+
+# one line test
+
+## till the field
+back_zero()
+for line_index in range(world_size):
+    safe_turn_to_soil()
+    move(East)
+
+while True:
+    back_zero()
+    ## plant sunflowers
+    for line_index in range(world_size):
+        plant(Entities.Sunflower)
+        water_the_field(0.8)
+        move(East)
+
+    ## record the energy dict
+    energy_list = []
+    back_zero()
+    for line_index in range(world_size):
+        energy_list.append(measure())
+        move(East)
+
+    # bubble sort the energy list
+
+
+    for index in range(len(energy_list)):
+        max_leaf = max(energy_list)
+        max_index = get_index(energy_list, max_leaf)
+
+        to_position((max_index,0))
+        while True:
+            if safe_harvest():
+                break
+
+        energy_list[max_index] = 0
