95dd26d02d8828e84a0d18e811ac0a30f08fbfdf
Adeepblue <2631634050@qq.com>
2025-12-15 10:50:43 +0800

    单列冒泡排序成功版本代码

--- temp.py
+++ temp.py
@@ -14,37 +14,22 @@ for line_index in range(get_world_size()):
 	till()
 	plant(Entities.Cactus)
 	move(East)
-
-# 不逆序
-not_reverse_flag = True
-
+	
+# bubble sort main loop
 pre_size = measure()
 for line_index in range(world_size):
-
-	# 从左到右
-	if not_reverse_flag:
-		for loop_num in range(world_size - line_index -1):
-			move(East)
-			print(measure())
-			now_size = measure()
-			if now_size < pre_size:
-				swap(West)
-			else:
-				pre_size = now_size
-		print("S")
-	# 从右到左
-	else:
-		for loop_num in range(world_size - line_index):
-			move(West)
-			print(measure())
-			now_size = measure()
-			if now_size > pre_size:
-				swap(East)
-			else:
-				pre_size = now_size
-		print("E")
-
-	not_reverse_flag = not not_reverse_flag
+	pre_size = measure()
+	print(measure())
+	for loop_num in range(world_size - line_index -1):
+		move(East)
+		print(measure())
+		now_size = measure()
+		if now_size < pre_size:
+			swap(West)
+		else:
+			pre_size = now_size
+	print("END")
+	to_position((0,0))
 
 harvest()
 
