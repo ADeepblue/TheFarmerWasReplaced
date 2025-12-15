c7ed9b5df36df4d027a8c8e3cd852831486fb8e4
Adeepblue <2631634050@qq.com>
2025-12-15 10:43:35 +0800

    弃用旧版to_position,采纳新版to_position_plus,重命名为to_position

--- utils.py
+++ utils.py
@@ -26,24 +26,7 @@ def till_all_field(size_num):
 			move(North)
 		move(East)
 
-def to_position(position):
-	x = position[0]
-	y = position[1]
-	x_now = get_pos_x()
-	y_now = get_pos_y()
-	if x_now < x:
-		for _ in range(x-x_now):
-			move(East)
-	elif x_now > x:
-		for _ in range(x_now-x):
-			move(West)
 
-	if y_now < y:
-		for _ in range(y-y_now):
-			move(North)
-	elif y_now > y:
-		for _ in range(y_now-y):
-			move(South)
 
 def water_the_field(limit_water_percent):
 	if get_water() <=limit_water_percent:
@@ -54,7 +37,7 @@ def safe_turn_to_soil():
 		safe_harvest()
 		till()
 
-def to_position_plus(position):
+def to_position(position):
 	x = position[0]
 	y = position[1]
 	x_now = get_pos_x()
