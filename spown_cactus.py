4e0ff35ba55d8a8f72bfe7482d2aad4cc95eb0d8
Adeepblue <2631634050@qq.com>
2025-12-15 19:54:40 +0800

    并行无人机收获仙人掌(未校验)

--- spown_cactus.py
+++ spown_cactus.py
@@ -2,7 +2,7 @@ from __builtins__ import *
 from utils import *
 
 # parameter setting
-Cactus_Water_Level = 0.6
+Cactus_Water_Level = 0
 
 # main
 clear()
@@ -11,17 +11,105 @@ world_size = get_world_size()
 
 # till the field
 
+
 def row_till_task():
-    for row_index in range(world_size):
-        safe_turn_to_soil()
-        water_the_field(Cactus_Water_Level)
-        move(North)
+	to_position((line_index,0))
+	for _ in range(world_size):
+		safe_turn_to_soil()
+		water_the_field(Cactus_Water_Level)
+		move(North)
 
 
 for line_index in range(world_size):
-    while True:
-        if spawn_drone(row_till_task):
-            move(East)
-            break
+	if num_drones() < max_drones():
+		spawn_drone(row_till_task)
+	else:
+		while True:
+			if num_drones() < max_drones():
+				spawn_drone(row_till_task)
+				break
 
+# plant the cactus
 
+def row_plant_task():
+	to_position((line_index,0))
+	for _ in range(world_size):
+		plant(Entities.Cactus)
+		move(North)
+
+back_zero()
+
+for line_index in range(world_size):
+	if num_drones() < max_drones():
+		spawn_drone(row_plant_task)
+	else:
+		while True:
+			if num_drones() < max_drones():
+				spawn_drone(row_plant_task)
+				break
+
+# bubble sort main loop
+
+# parallel task
+while True:
+	def bubble_sort_line_task():
+		#复位
+		to_position((0,line_index))
+		for row_index in range(world_size):
+			pre_size = measure()
+			for _ in range(world_size - row_index - 1):
+				move(East)
+				now_size = measure()
+				if now_size < pre_size:
+					swap(West)
+				else:
+					pre_size = now_size
+			# print("END")
+			to_position((0,line_index))
+
+	# bubble sort main loop in lines
+	for line_index in range(world_size):
+		if num_drones() < max_drones():
+			spawn_drone(bubble_sort_line_task)
+		else:
+			while True:
+				if num_drones() < max_drones():
+					spawn_drone(bubble_sort_line_task)
+					break
+
+	back_zero()
+
+	# bubble sort main loop in rows
+	while True:
+		if num_drones() == 1:
+			break
+	def bubble_sort_row_task():
+		# 复位
+		to_position((row_index,0))
+		for line_index in range(world_size):
+			pre_size = measure()
+			for _ in range(world_size - line_index - 1):
+				move(North)
+				now_size = measure()
+				if now_size < pre_size:
+					swap(South)
+				else:
+					pre_size = now_size
+			# print("END")
+			to_position((row_index,0))
+
+	# bubble sort main loop in rows
+	for row_index in range(world_size):
+		if num_drones() < max_drones():
+			spawn_drone(bubble_sort_row_task)
+		else:
+			while True:
+				if num_drones() < max_drones():
+					spawn_drone(bubble_sort_row_task)
+					break
+
+	while True:
+		if num_drones() == 1:
+			break
+
+	safe_harvest()
