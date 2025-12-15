86a544ab62e978c7bada1efdee7bfe805ec8b8af
Adeepblue <2631634050@qq.com>
2025-12-15 19:56:12 +0800

    太阳花收获能量

--- harvest_energy.py
+++ harvest_energy.py
@@ -12,34 +12,34 @@ world_size = get_world_size()
 ## till the field
 back_zero()
 for line_index in range(world_size):
-    safe_turn_to_soil()
-    move(East)
+	safe_turn_to_soil()
+	move(East)
+
+
+back_zero()
+## plant sunflowers
+for line_index in range(world_size):
+	plant(Entities.Sunflower)
+	move(East)
+
+## record the energy dict
+energy_list = []
+back_zero()
+for line_index in range(world_size):
+	energy_list.append(measure())
+	move(East)
+
+# bubble sort the energy list
 
 while True:
-    back_zero()
-    ## plant sunflowers
-    for line_index in range(world_size):
-        plant(Entities.Sunflower)
-        water_the_field(0.8)
-        move(East)
+	max_leaf = max(energy_list)
+	max_index = get_index(energy_list, max_leaf)
 
-    ## record the energy dict
-    energy_list = []
-    back_zero()
-    for line_index in range(world_size):
-        energy_list.append(measure())
-        move(East)
+	to_position((max_index,0))
+	while True:
+		if safe_harvest():
+			water_the_field(1)
+			plant(Entities.Sunflower)
+			break
 
-    # bubble sort the energy list
-
-
-    for index in range(len(energy_list)):
-        max_leaf = max(energy_list)
-        max_index = get_index(energy_list, max_leaf)
-
-        to_position((max_index,0))
-        while True:
-            if safe_harvest():
-                break
-
-        energy_list[max_index] = 0
+	energy_list[max_index] = measure()
