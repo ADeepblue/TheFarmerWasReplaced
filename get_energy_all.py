63b1af398817c12911242da4ab3cc0a196a21036
Adeepblue <2631634050@qq.com>
2025-12-16 09:19:12 +0800

    最终版本,收割能量

--- get_energy_all.py
+++ get_energy_all.py
@@ -4,7 +4,7 @@ from utils import *
 clear()
 
 world_size = get_world_size()
-Cactus_Water_Level = 0.6
+Cactus_Water_Level = 0.8
 
 # function to get all indices of a value in a list
 def get_index_list(list,value):
@@ -39,61 +39,47 @@ for line_index in range(world_size-1):
 
 line_till_task()
 
-# plant all the sunflowers
-back_zero()
+
+
+
+
+def loop_safe_harvest():
+    while True:
+        if safe_harvest():
+            break
+
 def line_plant_task():
     for _ in range(world_size):
         plant(Entities.Sunflower)
         move(East)
 
-for line_index in range(world_size):
-    tiny_sleep()
-    spawn_drone(line_plant_task)
-    move(North)
-
-line_plant_task()
-
-
-# build the energy list
-energy_list = []
-spawn_drone_list = []
-
-# init list
-for _ in range(world_size):
-    spawn_drone_list.append(None)
-
-for _ in range(world_size):
-    temp_list = []
-    energy_list.append(None)
-
-# record energy task
-def line_record_energy_task():
-    to_position((0,line_index))
-    temp_list = []
-    for row_index in range(world_size):
-        temp_list.append(measure())
+def line_harvest_task():
+    for _ in range(world_size):
+        loop_safe_harvest()
         move(East)
 
-    return temp_list
 
-# 放飞所有无人机
-for row_index in range(world_size-1):
-    tiny_sleep()
-    spawn_drone_list[row_index] = spawn_drone(line_record_energy_task)
-    move(North)
-# 最后一条线自己执行
 
-energy_list[world_size-1] = line_record_energy_task()
+# main loop
+while True:
+# plant all the sunflowers
+    back_zero()
 
-# line_record_energy_task()
 
-tiny_sleep()
+    for _ in range(world_size-1):
+        tiny_sleep()
+        spawn_drone(line_plant_task)
+        water_the_field(Cactus_Water_Level)
+        move(North)
 
-for line_index in range(1,world_size):
-    if has_finished(spawn_drone_list[line_index]):
-        temp_list = wait_for(spawn_drone_list[line_index])
-        energy_list[line_index] = temp_list
+    line_plant_task()
 
-quick_print(energy_list)
+
+    for _ in range(world_size-1):
+        tiny_sleep()
+        spawn_drone(line_harvest_task)
+        move(North)
+
+    line_harvest_task()
 
 
