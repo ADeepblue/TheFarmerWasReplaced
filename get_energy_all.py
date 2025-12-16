c88ae6395429fe30eeb834d42c6c8600e80ddb0f
Adeepblue <2631634050@qq.com>
2025-12-16 14:19:47 +0800

    向日葵能量收获in parallel,函数未分离

--- get_energy_all.py
+++ get_energy_all.py
@@ -1,6 +1,10 @@
 from __builtins__ import *
 from utils import *
 # main
+
+back_zero()
+harvest_all_spawn()
+
 clear()
 
 world_size = get_world_size()
@@ -8,34 +12,34 @@ Cactus_Water_Level = 0.8
 
 # function to get all indices of a value in a list
 def get_index_list(list,value):
-    index_list = []
-    for index in range(len(list)):
-        if list[index] == value:
-            index_list.append(index)
-    if index_list != []:
-        return index_list
-    else:
-        return None
+	index_list = []
+	for index in range(len(list)):
+		if list[index] == value:
+			index_list.append(index)
+	if index_list != []:
+		return index_list
+	else:
+		return None
 
 # sleep
 
 def tiny_sleep():
-    move(North)
-    move(South)
+	move(North)
+	move(South)
 
 # till all the field
 back_zero()
 def line_till_task():
-    for _ in range(world_size):
-        safe_turn_to_soil()
-        water_the_field(Cactus_Water_Level)
-        move(East)
+	for _ in range(world_size):
+		safe_turn_to_soil()
+		water_the_field(Cactus_Water_Level)
+		move(East)
 
 
 for line_index in range(world_size-1):
-    tiny_sleep()
-    spawn_drone(line_till_task)
-    move(North)
+	tiny_sleep()
+	spawn_drone(line_till_task)
+	move(North)
 
 line_till_task()
 
@@ -44,42 +48,43 @@ line_till_task()
 
 
 def loop_safe_harvest():
-    while True:
-        if safe_harvest():
-            break
+	while True:
+		if safe_harvest():
+			break
 
 def line_plant_task():
-    for _ in range(world_size):
-        plant(Entities.Sunflower)
-        move(East)
+	for _ in range(world_size):
+		plant(Entities.Sunflower)
+		water_the_field(Cactus_Water_Level)
+		move(East)
 
 def line_harvest_task():
-    for _ in range(world_size):
-        loop_safe_harvest()
-        move(East)
+	for _ in range(world_size):
+		loop_safe_harvest()
+		move(East)
 
 
 
 # main loop
 while True:
 # plant all the sunflowers
-    back_zero()
+	back_zero()
 
 
-    for _ in range(world_size-1):
-        tiny_sleep()
-        spawn_drone(line_plant_task)
-        water_the_field(Cactus_Water_Level)
-        move(North)
+	for _ in range(world_size-1):
+		tiny_sleep()
+		spawn_drone(line_plant_task)
+		water_the_field(Cactus_Water_Level)
+		move(North)
 
-    line_plant_task()
+	line_plant_task()
 
 
-    for _ in range(world_size-1):
-        tiny_sleep()
-        spawn_drone(line_harvest_task)
-        move(North)
+	for _ in range(world_size-1):
+		tiny_sleep()
+		spawn_drone(line_harvest_task)
+		move(North)
 
-    line_harvest_task()
+	line_harvest_task()
 
 
