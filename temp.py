22976a8936ee4727afbee21cf4ca9bfd1e059a7b
Adeepblue <2631634050@qq.com>
2025-12-22 09:33:32 +0800

    无人机外部改变变量影响内部确认,但是内部如果不传递变量出来,无法影响外部变量

--- temp.py
+++ temp.py
@@ -7,7 +7,7 @@ from utils import *
 # main
 # clear()
 #
-world_size = 8
+world_size = 32
 set_world_size(world_size)
 # till the field
 
@@ -15,16 +15,70 @@ def plant_Carrot():
 	plant(Entities.Carrot)
 	water_the_field(Cautious_Water_Level)
 
-till_in_parallel()
-for_all(plant_Carrot)
+# till_in_parallel()
+# for_all(plant_Carrot)
+#
+# def wait_for_all_drones_finished():
+# 	while True:
+# 		if num_drones() == 1:
+# 			break
+#
+# wait_for_all_drones_finished()
+#
+# harvest_all_spawn_loop()
+# while True:
+# 	do_a_flip()
 
-def wait_for_all_drones_finished():
-	while True:
-		if num_drones() == 1:
-			break
+# def line_plant_task():
+# 	for _ in range(world_size):
+# 		plant_Carrot()
+# 		move(East)
+#
+# for _ in range(world_size-1):
+# 	wait_for(spawn_drone(line_plant_task))
+# 	move(North)
+#
+# line_plant_task()
 
-wait_for_all_drones_finished()
+# def row_harvest_task():
+# 	for _ in range(get_world_size()):
+# 		safe_harvest()
+# 		move(North)
 
-harvest_all_spawn_loop()
-while True:
-	do_a_flip()
+
+flag = 0
+
+
+drone_handles = []
+def row_harvest_task():
+	# 示例行任务：横向遍历一行，收割并向东方向移动
+	global drone_handles
+	global world_size
+	global flag
+	for _ in range(get_world_size()):
+		safe_harvest()
+		move(East)
+
+	print(flag)
+	if num_drones() < max_drones():
+		tiny_sleep()
+		flag += 1
+		spawn_drone(row_harvest_task)
+
+
+
+
+
+
+
+# while True:
+for _ in range(max_drones()):
+	if num_drones() < max_drones():
+		tiny_sleep()
+		flag += 1
+		handle = spawn_drone(row_harvest_task)
+
+		# drone_handles.append(handle)
+		move(North)
+	else:
+		row_harvest_task()
