3ea70fe3215b08f77f5cf892385d9b055fd56d9b
Adeepblue <2631634050@qq.com>
2025-12-23 13:42:29 +0800

    草的批量收获版本,但似乎不可行

--- second_temp_done.py
+++ second_temp_done.py
@@ -2,8 +2,41 @@ from __builtins__ import *
 from utils import *
 
 # parameter setting
+Cactus_Water_Level = 0.5
 world_size = 32
 
-back_zero()
+# set world
+# set_world_size(world_size)
 
-safe_harvest()
+# init
+clear()
+def plant_bush_and_tree():
+	x,y = get_position()
+	if (x+y) % 2 ==0:
+		# plant(Entities.Tree)
+		water_the_field(Cactus_Water_Level)
+	else:
+		# plant(Entities.Bush)
+		water_the_field(Cactus_Water_Level)
+
+
+
+back_zero()
+def line_plant_task():
+	while True:
+		for _ in range(world_size):
+			safe_harvest()
+			plant_bush_and_tree()
+			move(East)
+
+
+for _ in range(world_size-1):
+	tiny_sleep()
+	spawn_drone(line_plant_task)
+	move(North)
+
+line_plant_task()
+
+
+
+
