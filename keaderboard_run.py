d59d8951e00d86ef792da7b6e03d22326adf42b1
Adeepblue <2631634050@qq.com>
2025-12-27 14:21:05 +0800

    重命名leaderboard排行榜文件

--- keaderboard_run.py
+++
@@ -1,4 +0,0 @@
-from __builtins__ import *
-filename = "keyboard_pumpkin"
-speed = 10000
-runtime = leaderboard_run(Leaderboards.Pumpkins,filename,speed)
---
+++ leaderboard_run.py
@@ -0,0 +1,4 @@
+from __builtins__ import *
+filename = "keyboard_pumpkin"
+speed = 1
+runtime = leaderboard_run(Leaderboards.Pumpkins,filename,speed)
