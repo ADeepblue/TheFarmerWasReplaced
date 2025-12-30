19b9584b6a3c7ec265220eeb78233d8c01e11e7a
Adeepblue <2631634050@qq.com>
2025-12-30 11:07:21 +0800

    生成希尔伯特曲线,预加载版本

--- temp.py
+++ temp.py
@@ -10,212 +10,61 @@ half_bad_pumpkin_list = []
 # init the position information,clock_wise
 clear()
 
-East_position_list_CCW = [(1,1),(2,3),(3,1),(4,3),(5,1)]
-West_postion_list_CCW = [(2,6),(3,4),(4,6),(5,4),(6,6)]
-South_position_list_CCW = [(1,2),(1,3),(1,4),(1,5),(1,6),(3,2),(3,3),(3,5),(3,6),(5,2),(5,3),(5,5),(5,6)]
-North_position_list_CCW = [(2,1),(2,2),(2,4),(2,5),(4,1),(4,2),(4,4),(4,5),(6,1),(6,2),(6,3),(6,4),(6,5)]
 
-East_position_list_CW = [(1,6),(2,4),(3,6),(4,4),(5,6)]
-West_postion_list_CW = [(2,1),(3,3),(4,1),(5,3),(6,1)]
-South_position_list_CW = [(2,2),(2,3),(2,5),(2,6),(4,2),(4,3),(4,5),(4,6),(6,2),(6,3),(6,4),(6,5),(6,6)]
-North_position_list_CW = [(1,1),(1,2),(1,3),(1,4),(1,5),(3,1),(3,2),(3,4),(3,5),(5,1),(5,2),(5,4),(5,5)]
+def generate_hilbert_path(n):
+	"""基于你的正确实现生成路径"""
+	path = []
+	position = [0, 0]
 
-# init
-def not_false_true(bool_symbol):
-	if bool_symbol:
-		return False
-	elif not bool_symbol:
-		return True
-	else:
-		return None
+	up = [0, 1]
+	down = [0, -1]
+	left = [-1, 0]
+	right = [1, 0]
 
+	def move_vec(list_1, list_2):
+		return [list_1[0] + list_2[0], list_1[1] + list_2[1]]
 
-def check_position_in_list(position, position_list):
-	x = position[0]
-	y = position[1]
-	x_mod = x % 8
-	y_mod = y % 8
-	if (x_mod, y_mod) in position_list:
-		return True
-	else:
-		return False
+	def record_move(direction):
+		nonlocal position
+		position = move_vec(position, direction)
+		path.append(tuple(position))
 
-def check_CCW_direction(position):
-	if check_position_in_list(position,East_position_list_CCW):
-		return East
-
-	elif check_position_in_list(position,West_postion_list_CCW):
-		return West
-
-	elif check_position_in_list(position,North_position_list_CCW):
-		return North
-
-	elif check_position_in_list(position,South_position_list_CCW):
-		return South
-
-
-def check_CW_direction(position):
-	if check_position_in_list(position,East_position_list_CW):
-		return East
-
-	elif check_position_in_list(position,West_postion_list_CW):
-		return West
-
-	elif check_position_in_list(position,North_position_list_CW):
-		return North
-
-	elif check_position_in_list(position,South_position_list_CW):
-		return South
-
-def sub_drone_till_and_plant_task():
-	global up_flag
-
-	for _ in range(18):
-		only_turn_to_soil()
-		plant(Entities.Pumpkin)
-		if get_water() <= Cautious_Water_Level:
-			use_item(Items.Water)
-
-		if up_flag:
-			direction = check_CW_direction(get_position())
-			move(direction)
+	def hilbert(n, up, right, down, left):
+		if n == 1:
+			record_move(up)  # 第一步：向上
+			record_move(right)  # 第二步：向右
+			record_move(down)  # 第三步：向下
 		else:
-			direction = check_CCW_direction(get_position())
-			move(direction)
+			hilbert(n - 1, right, up, left, down)
+			record_move(up)
+			hilbert(n - 1, up, right, down, left)
+			record_move(right)
+			hilbert(n - 1, up, right, down, left)
+			record_move(down)
+			hilbert(n - 1, left, down, right, up)
 
-def sub_plant_task():
-	global up_flag
+	path.append(tuple(position))  # 起点 (0, 0)
+	hilbert(n, up, right, down, left)
 
-	for _ in range(18):
-		plant(Entities.Pumpkin)
-		if get_water() <= Cautious_Water_Level:
-			use_item(Items.Water)
-
-		if up_flag:
-			direction = check_CW_direction(get_position())
-			move(direction)
-		else:
-			direction = check_CCW_direction(get_position())
-			move(direction)
+	return path
 
 
-def sub_check_bad_pumpkin():
-	global up_flag
-	bad_pumpkin_list = []
-	for _ in range(18):
-		if not can_harvest():
-			plant(Entities.Pumpkin)
-			bad_pumpkin_list.append(get_position())
-		safe_water_the_field(Cautious_Water_Level)
 
-		if up_flag:
-			direction = check_CW_direction(get_position())
-			move(direction)
-		else:
-			direction = check_CCW_direction(get_position())
-			move(direction)
 
-	return bad_pumpkin_list
+# 生成32x32的5阶希尔伯特曲线
+hilbert_path = generate_hilbert_path(5)  # 2^5 = 32
+print(get_tick_count())
+# print(f"路径长度: {len(hilbert_path)}")  # 应该是 1024
 
-def sub_kill_bad_pumpkin():
-	global half_bad_pumpkin_list
+def temp_task():
+	to_position(hilbert_path[index*32])
 	while True:
-		if num_items(Items.Pumpkin) >= 200000000:
-			break
+		print(num_drones())
+for index in range(1,32):
+	# print(hilbert_path[index*32])
+	spawn_drone(temp_task)
 
-		temp_list = []
-		for position in half_bad_pumpkin_list:
-			to_position(position)
-			if not can_harvest():
-				plant(Entities.Pumpkin)
-				if len(half_bad_pumpkin_list) ==1 and num_items(Items.Fertilizer) >= 2:
-					# use_item(Items.Weird_Substance)
-					use_item(Items.Fertilizer)
-					# use_item(Items.Weird_Substance)
-				temp_list.append(position)
-
-		half_bad_pumpkin_list = temp_list
-
-		if len(temp_list) == 0:
-			break
-
-
-def main_task():
-	global up_flag
-	global half_bad_pumpkin_list
-	start_postion = (index2*8+1,index1*8+3)
-	to_position(start_postion)
-	# (1,3)
-	# 负责下半部分
-	up_flag = True
-	move(North)
-	spawn_drone(sub_drone_till_and_plant_task)
-
-	move(South)
-	up_flag = False
-	sub_drone_till_and_plant_task()
-
-	while True:
-
-		if num_items(Items.Pumpkin) >= 200000000:
-			break
-		# check bad pumpkin
-		to_position(start_postion)
-		up_flag = True
-		move(North)
-		sub_drone = spawn_drone(sub_check_bad_pumpkin)
-
-		move(South)
-		up_flag = False
-		main_bad_pumpkin_list = sub_check_bad_pumpkin()
-
-		while True:
-			if has_finished(sub_drone):
-				half_bad_pumpkin_list = wait_for(sub_drone)
-				break
-
-
-		sub_drone = spawn_drone(sub_kill_bad_pumpkin)
-
-
-
-		while True:
-			temp_list = []
-			for position in main_bad_pumpkin_list:
-				to_position(position)
-				if not can_harvest():
-					plant(Entities.Pumpkin)
-					if len(half_bad_pumpkin_list) == 1 and num_items(Items.Fertilizer) >= 2:
-						use_item(Items.Weird_Substance)
-						use_item(Items.Fertilizer)
-						use_item(Items.Weird_Substance)
-
-					temp_list.append(position)
-
-			main_bad_pumpkin_list = temp_list
-
-			if len(temp_list) == 0:
-				break
-
-		while True:
-			if has_finished(sub_drone):
-				wait_for(sub_drone)
-				break
-
-		safe_harvest()
-
-
-def quick_print_position():
-	print(get_position())
-
-to_position((0,2))
-
-
-
-for index1 in range(4):
-	for index2 in range(4):
-		tiny_sleep()
-		if not ((index1==3) and (index2==3)):
-			spawn_drone(main_task)
-
-main_task()
+print(num_drones())
+to_position((0,0))
+while True:
+	print(num_drones())
