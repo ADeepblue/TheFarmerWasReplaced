0719adf6357f529faa55d020ff72ba6254c55313
Adeepblue <2631634050@qq.com>
2025-12-31 23:31:01 +0800

    胡萝卜混种最终版本

---
+++ mix_plant_carrot_all.py
@@ -0,0 +1,93 @@
+from __builtins__ import *
+from utils import *
+
+# parameter setting
+Cactus_Water_Level = 1
+
+# init up_flag
+up_flag = False
+half_bad_pumpkin_list = []
+# init the position information,clock_wise
+clear()
+
+
+def safe_plant_Carrot():
+	harvest()
+	if get_ground_type() != Grounds.Soil:
+		till()
+
+	plant(Entities.Carrot)
+
+def safe_plant_grass():
+	harvest()
+	if get_ground_type() != Grounds.Grassland:
+		till()
+
+def safe_plant_bush():
+	harvest()
+	plant(Entities.Bush)
+
+def safe_plant_tree():
+	harvest()
+	plant(Entities.Tree)
+
+def safe_plant(Plant):
+	if Plant == Entities.Carrot:
+		safe_plant_Carrot()
+
+	elif Plant == Entities.Grass:
+		safe_plant_grass()
+
+	elif Plant == Entities.Bush:
+		safe_plant_bush()
+
+	elif Plant == Entities.Tree:
+		safe_plant_tree()
+
+	else:
+		return None
+
+def mix_main_task():
+	global position
+	to_position(position)
+	position_list = []
+	direction_list = [South,West,North,East]
+	for direction in direction_list:
+		position_list.append(get_position())
+		safe_plant(Entities.Carrot)
+		safe_water_the_field(Cactus_Water_Level)
+		move(direction)
+
+	while True:
+		for position in position_list:
+			safe_water_the_field(Cactus_Water_Level)
+			to_position(position)
+			if get_entity_type() != Entities.Carrot:
+				safe_plant_Carrot()
+				continue
+			if get_companion() == None:
+				safe_harvest()
+			else:
+				companion_plant, position_temp = get_companion()
+				to_position(position_temp)
+				safe_plant(companion_plant)
+				to_position(position)
+				loop_safe_harvest()
+				safe_plant_Carrot()
+
+
+for index1 in range(4):
+	for index2 in range(4):
+		position = (index1*8,index2*8)
+		spawn_drone(mix_main_task)
+
+for index1 in range(4):
+	for index2 in range(4):
+		if (index1 == 3) and (index2 ==3):
+			continue
+		else:
+			position = (index1*8+4,index2*8+4)
+			spawn_drone(mix_main_task)
+
+position = (index1*8+4,index2*8+4)
+mix_main_task()
