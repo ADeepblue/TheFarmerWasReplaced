3a3848c2152be0e923d319747cf1569ade99c435
Adeepblue <2631634050@qq.com>
2026-01-12 11:38:33 +0800

    simulate main file

--- simulate_test.py
+++ simulate_test.py
@@ -4,9 +4,10 @@ from utils import *
 # parameters
 filename = "temp"
 sim_unlocks = Unlocks
-base_num = 10000
-sim_items = {Items.Carrot:base_num,Items.Hay:base_num,Items.Fertilizer:1000000000,Items.Water:1000000000,Items.Power:1000000000,Items.Carrot:10000000000}
+base_num = 0
+sim_items = {Items.Carrot:base_num,Items.Hay:base_num,Items.Fertilizer:1000000000,Items.Water:1000000000,Items.Power:1000000000,Items.Cactus:base_num}
 sim_global = {'Cactus_Water_Level' : 0.5,'world_size' : 32}
-seed=0
-speedup=10
+seed=5
+speedup=50000
 runtime = simulate(filename,sim_unlocks,sim_items,sim_global,seed,speedup)
+# quick_print(get_tick_count())
