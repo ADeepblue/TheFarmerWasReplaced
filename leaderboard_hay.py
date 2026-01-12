15eb6ae10df7dd59d7abe6d1164df07b88936699
Adeepblue <2631634050@qq.com>
2026-01-12 11:36:26 +0800

    leaderboard of hay

--- leaderboard_hay.py
+++ leaderboard_hay.py
@@ -2,7 +2,7 @@ from __builtins__ import *
 from utils import *
 
 # parameter setting
-Cactus_Water_Level =0.5
+Cactus_Water_Level =0.3
 
 # init up_flag
 up_flag = False
@@ -59,23 +59,23 @@ def mix_main_task():
 		# safe_water_the_field(Cactus_Water_Level)
 		# move(direction)
 
-	position_list.append(get_position())
+	# position_list.append(get_position())
 
 	while True:
-		for position in position_list:
+	# for position in position_list:
+		to_position(position)
+		safe_water_the_field(Cactus_Water_Level)
+		if get_entity_type() != Entities.Grass:
+			safe_plant_grass()
+			continue
+		# if get_companion() == None:
+		# 	safe_harvest()
+		else:
+			companion_plant, position_temp = get_companion()
+			to_position(position_temp)
+			safe_plant(companion_plant)
 			to_position(position)
-			safe_water_the_field(Cactus_Water_Level)
-			if get_entity_type() != Entities.Grass:
-				safe_plant_grass()
-				continue
-			if get_companion() == None:
-				safe_harvest()
-			else:
-				companion_plant, position_temp = get_companion()
-				to_position(position_temp)
-				safe_plant(companion_plant)
-				to_position(position)
-				loop_safe_harvest()
+			loop_safe_harvest()
 
 		if num_items(Items.Hay) >= 2000000000:
 			break
